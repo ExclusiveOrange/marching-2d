7621f24633f12f7c896ed0e9c75c02726ec526dc
Atlee Brink <atlee@atleebrink.com>
2022-01-25 19:21:45 -0800

    about to start exploration on 'dual contouring'

--- MainWindow.cs
+++ MainWindow.cs
@@ -120,5 +120,20 @@ namespace marching_2d
       pictureBoxTriangles.SizeMode = PictureBoxSizeMode.Zoom;
       Controls.Add(pictureBoxTriangles);
     }
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // MainWindow
+      // 
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Name = "MainWindow";
+      this.ResumeLayout(false);
+    }
   }
 }
