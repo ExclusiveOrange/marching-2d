09f0ddab7807e9ad62f373033c497294edde0e4b
Atlee Brink <atlee@atleebrink.com>
2022-01-26 12:44:42 -0800

    added image for dual contouring

--- MainWindow.cs
+++ MainWindow.cs
@@ -9,9 +9,12 @@ namespace marching_2d
   {
     private const int imageWidth = 1000;
     private const int imageHeight = 1000;
+
     private const PixelFormat imageFormat = PixelFormat.Format32bppRgb;
-    private readonly Bitmap imageRectangles = new Bitmap(imageWidth, imageHeight, imageFormat);
-    private readonly Bitmap imageTriangles = new Bitmap(imageWidth, imageHeight, imageFormat);
+
+    private readonly Bitmap imageRectangles = new(imageWidth, imageHeight, imageFormat);
+    private readonly Bitmap imageTriangles = new(imageWidth, imageHeight, imageFormat);
+    private readonly Bitmap imageDual = new(imageWidth, imageHeight, imageFormat);
 
     private const float xScale = 5f;
     private const float xOffset = 0f;
@@ -21,7 +24,7 @@ namespace marching_2d
     private const float rXScale = 1.0f / xScale;
     private const float rYScale = 1.0f / yScale;
 
-    private readonly FastNoiseLite fastNoise = new FastNoiseLite();
+    private readonly FastNoiseLite fastNoise = new();
     private const int seed = 1337;
 
     private static
@@ -42,11 +45,13 @@ namespace marching_2d
       for (int y = 0; y < imageHeight; ++y)
         for (int x = 0; x < imageWidth; ++x)
         {
-          float fieldValue = FieldValueAt(x, y);
-          float scaled = clamp(0.5f + 5f * fieldValue, 0f, 1f);
-          int b = (int) (255f * scaled);
+          float v = FieldValueAt(x, y);
+          float n = clamp(0.5f + 5f * v, 0f, 1f);
+          int b = (int) (255f * n);
+
           imageRectangles.SetPixel(x, y, Color.FromArgb(b, b, b));
           imageTriangles.SetPixel(x, y, Color.FromArgb(b, b, b));
+          imageDual.SetPixel(x, y, Color.FromArgb(b, b, b));
         }
 
       Pen pen = new Pen(Color.Fuchsia, 2);
@@ -56,6 +61,9 @@ namespace marching_2d
 
       using (var graphics = Graphics.FromImage(imageTriangles))
         RenderTriangles(pen, graphics);
+
+      using (var graphics = Graphics.FromImage(imageDual))
+        RenderDual(pen, graphics);
     }
 
     private
@@ -63,7 +71,7 @@ namespace marching_2d
       FieldValueAt(float x, float y)
     {
       const float primitiveScale = 100f;
-      
+
       var noiseValue = fastNoise.GetNoise(x * rXScale + xOffset, y * rYScale + yOffset);
 
       float circleValue = 0f;
@@ -98,61 +106,90 @@ namespace marching_2d
 
     private
       void
-      RenderRectangles(Pen pen, Graphics graphics)
+      RenderDual(Pen pen, Graphics graphics)
     {
-      MarchingRectangles.Draw(
-        new MarchingRectangles.DrawParameters
-        {
-          pen = pen,
-          graphics = graphics,
-          imageWidth = imageWidth,
-          imageHeight = imageHeight,
-          gridWidth = 20,
-          gridHeight = 20,
-          fieldFunction = FieldValueAt
-        });
+      // throw new NotImplementedException();
     }
 
     private
       void
+      RenderRectangles(Pen pen, Graphics graphics)
+      =>
+        MarchingRectangles.Draw(
+          new MarchingRectangles.DrawParameters
+          {
+            pen = pen,
+            graphics = graphics,
+            imageWidth = imageWidth,
+            imageHeight = imageHeight,
+            gridWidth = 20,
+            gridHeight = 20,
+            fieldFunction = FieldValueAt
+          });
+
+    private
+      void
       RenderTriangles(Pen pen, Graphics graphics)
-    {
-      MarchingTriangles.Draw(
-        new MarchingTriangles.DrawParameters
-        {
-          pen = pen,
-          graphics = graphics,
-          imageWidth = imageWidth,
-          imageHeight = imageHeight,
-          triangleSideLength = 30,
-          fieldFunction = FieldValueAt
-        });
-    }
+      =>
+        MarchingTriangles.Draw(
+          new MarchingTriangles.DrawParameters
+          {
+            pen = pen,
+            graphics = graphics,
+            imageWidth = imageWidth,
+            imageHeight = imageHeight,
+            triangleSideLength = 50,
+            fieldFunction = FieldValueAt
+          });
 
     private
       void
       SetupUi()
     {
       AutoScaleMode = AutoScaleMode.Font;
-      ClientSize = new Size(imageWidth * 2, imageHeight);
+      ClientSize = new Size(imageWidth * 3, imageHeight);
       FormBorderStyle = FormBorderStyle.FixedSingle;
       Text = @"Marching 2D";
-
+      
+      FlowLayoutPanel layout = new();
+      layout.FlowDirection = FlowDirection.LeftToRight;
+      layout.WrapContents = false;
+      layout.Dock = DockStyle.Fill;
+      layout.Padding = new Padding(0);
+      layout.Margin = new Padding(0);
+      
       var pictureBoxRectangles = new PictureBox();
       pictureBoxRectangles.Size = new Size(imageWidth, imageHeight);
       pictureBoxRectangles.Image = imageRectangles;
       pictureBoxRectangles.Dock = DockStyle.Left;
+      pictureBoxRectangles.Margin = new Padding(0);
       // pictureBoxRectangles.Anchor = AnchorStyles.Left | AnchorStyles.Top;
       pictureBoxRectangles.SizeMode = PictureBoxSizeMode.Zoom;
-      Controls.Add(pictureBoxRectangles);
+      // Controls.Add(pictureBoxRectangles);
+      layout.Controls.Add(pictureBoxRectangles);
+
+      var pictureBoxDual = new PictureBox();
+      pictureBoxDual.Size = new Size(imageWidth, imageHeight);
+      pictureBoxDual.Image = imageDual;
+      pictureBoxDual.Dock = DockStyle.Fill;
+      pictureBoxDual.Anchor = AnchorStyles.None;
+      pictureBoxDual.Margin = new Padding(0);
+      // pictureBoxTriangles.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+      pictureBoxDual.SizeMode = PictureBoxSizeMode.Zoom;
+      // Controls.Add(pictureBoxDual);
+      layout.Controls.Add(pictureBoxDual);
 
       var pictureBoxTriangles = new PictureBox();
       pictureBoxTriangles.Size = new Size(imageWidth, imageHeight);
       pictureBoxTriangles.Image = imageTriangles;
       pictureBoxTriangles.Dock = DockStyle.Right;
+      pictureBoxTriangles.Margin = new Padding(0);
       // pictureBoxTriangles.Anchor = AnchorStyles.Right | AnchorStyles.Top;
       pictureBoxTriangles.SizeMode = PictureBoxSizeMode.Zoom;
-      Controls.Add(pictureBoxTriangles);
+      // Controls.Add(pictureBoxTriangles);
+      layout.Controls.Add(pictureBoxTriangles);
+      
+      Controls.Add(layout);
     }
 
     /// <summary>
